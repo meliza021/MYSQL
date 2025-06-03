82e9bc6e5048dbfab7c752d87d3980fcfb4170f8
meliza021 <189984449+meliza021@users.noreply.github.com>
2025-06-03 15:31:11 -0500

    punto numero 8

--- CONSULTAS.sql
+++ CONSULTAS.sql
@@ -1,10 +1,3 @@
--- 1.Consulta todos los datos de la tabla `usuarios` para ver la lista completa de clientes.
-SELECT * FROM usuarios;
-SELECT * FROM usuarios
-WHERE tipo_id = 1;
--- Muestra los nombres y correos electrónicos de todos los clientes que residen en la ciudad de Madrid.
-SELECT nombre,email FROM usuarios
-WHERE ciudad = 'Madrid';
 
 -- Obtén una lista de productos con un precio mayor a $100.000, mostrando solo el nombre y el precio.
 SELECT nombre,precio  FROM productos
@@ -30,3 +23,5 @@ WHERE  estado = 'pendiente'
 SELECT nombre, precio
 FROM productos
 WHERE precio = (SELECT MAX(precio) FROM productos);
+-- Obtén el total de pedidos realizados por cada cliente, mostrando el ID del cliente y el total de pedidos.
+SELECT estado, FROM pedidos
